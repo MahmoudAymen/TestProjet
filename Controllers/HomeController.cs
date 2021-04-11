263a58b1c88a9c57528c9806629a769a2aa7d3a9
MahmoudAymen <35262171+MahmoudAymen@users.noreply.github.com>
2021-04-11 22:28:21 +0100

    commit change

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace PushProjectGithub.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Hello Aymen.";
 
             return View();
         }
